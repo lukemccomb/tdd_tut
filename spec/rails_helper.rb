edb85feb11b5da13a1952826f5f10401ddd091b3
Luke McComb <lucasmccomb@gmail.com>
2017-03-16 20:53:48 -0600

    User can sign in (kind of)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,5 +36,7 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+
+  config.include Features, type: :feature
   config.infer_spec_type_from_file_location!
 end
