acf4322f5d68252cf6b09f9c97243a0db3dc4efb
Luke McComb <lucasmccomb@gmail.com>
2017-03-17 21:01:27 -0600

    Creates user methods for signed_in and todos which through AR also allows for todos.create method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,20 +4,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate
-    if !signed_in
+    if !signed_in?
       redirect_to new_session_path
     end
   end
 
-  def signed_in
-    current_email.present?
-  end
-
-  def current_email
-    session[:current_email]
+  def signed_in?
+    current_user.signed_in?
   end
 
   def sign_in_as(email)
     session[:current_email] = email
+  end
+
+  def current_user
+    User.new(session[:current_email])
   end
 end
