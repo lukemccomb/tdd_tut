6c4a44a70babe592a482c7f46bef9f66ed73cf95
Luke McComb <lucasmccomb@gmail.com>
2017-03-18 10:55:35 -0600

    Adds functionality for marking todo as incomplete

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -13,12 +13,24 @@ end
 
 describe Todo, "#complete!" do
   it "update completed_at" do
-    todo = Todo.create!(created_at: nil)
+    todo = Todo.create!(completed_at: nil)
 
     todo.complete!
 
     todo.reload
 
     expect(todo).to be_completed
+  end
+end
+
+describe Todo, "#mark_incomplete!" do
+  it "sets completed_at to nil" do
+    todo = Todo.create!(completed_at: Time.current)
+
+    todo.mark_incomplete!
+
+    todo.reload
+
+    expect(todo).not_to be_completed
   end
 end
